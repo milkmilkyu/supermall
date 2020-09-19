522b6bf7b18ee63253d8f517da6bb39cf467aee7
milkmilkyu <471425749@qq.com>
2020-09-19 11:17:04 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red"
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      // Vue-router 不允许导航到当前位置（/path），直接在跳转报错的那个地方加上.catch(error => error)
+      this.$router.replace(this.path).catch(error => error);
+    }
+  }
+};
+</script>
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  height: 49px;
+  text-align: center;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+}
+</style>
---
+++ src/views/home/childcomps/FeatureView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "FeatureView",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {}
+};
+</script>
+<style scoped></style>
